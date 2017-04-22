2a1ff00904b4eccf65c81c9619f9823843c3ea28
pablo <pablo@DESKTOP-15LHFP6>
2017-04-22 11:37:45 +0100

    Add project files.

---
+++ CVBotWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CVBotWeb.MvcApplication" Language="C#" %>
---
+++ CVBotWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CVBotWeb.Startup))]
+namespace CVBotWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
